aba552a0bca031cee1d751026c594789c53124c5
David Coats <david@getsafetycone.com>
2020-11-19 19:58:49 -0800

    Add R5T.T0012.Abstractions types library, with IEvent interface type

---
+++ source/R5T.T0012.Abstractions/Code/Interfaces/IEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace R5T.T0012.Abstractions
+{
+    public interface IEvent
+    {
+        Guid Identity { get; }
+        DateTime OccurredUTC { get; }
+        string TypeName { get; set; }
+    }
+}
